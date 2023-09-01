d794cc94cd637e9d3e2d3756c7f21534675325ce
whr1234567 <1024545747@qq.com>
2023-09-01 18:54:01 +0800

    修改test.c

--- test.c
+++ test.c
@@ -3,7 +3,19 @@
 
 int main(void)
 {
-	printf("test...\n");
+	
+	
+	printf("test1...\n");
+	printf("test2...\n");
+	printf("test3...\n");
+	printf("test4...\n");
+	
+	printf("test5...\n");
+	printf("test6...\n");
+	printf("test7...\n");
+
+
+
 	return 0;
 }
 
